297413abfbe1885a6ad3e15e2fc8d9e8bc0d9a51
devonk15 <kaylordevon5@gmail.com>
2024-11-06 20:11:27 -0500

    commit for now, its all not wanting to work :sob:

--- javascript/temp.js
+++ javascript/temp.js
@@ -52,6 +52,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const h1 = document.getElementById('header');
     const ask = document.getElementById('ask');
+    const ask2 = document.getElementById('ask2');
+
+    ask2.style.fontSize = '24px';
+    ask2.style.paddingLeft = '0px';
 
     ask.style.fontSize = '24px';
     ask.style.paddingLeft = '0px';
@@ -62,22 +66,35 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const div = document.getElementById('div');
     const input = document.getElementById('input');
+    const input2 = document.getElementById('input2');
+    const input3 = document.getElementById('input3');
     const btn = document.getElementById('btn');
-    const input4 = document.getElementById('input2');
     const btn2 = document.getElementById('btn2');
+    const btn3 = document.getElementById('btn3');
 
-    input4.style.visibility = 'hidden'; 
+    input3.style.visibility = 'hidden';
+    btn3.style.visibility = 'hidden';
+
+    input3.style.width = '150px';
+    input3.style.height = '24px';
+    input3.style.fontSize = '24px';
+
+    btn3.style.height = '30px';
+    btn3.style.fontSize = '15px';
+
+    input2.style.visibility = 'hidden'; 
     btn2.style.visibility = 'hidden';
-    input4.style.width = '75px';
-    input4.style.height = '24px';
-    input4.style.fontSize = '24px';
+
+    input2.style.width = '150px';
+    input2.style.height = '24px';
+    input2.style.fontSize = '24px';
 
     btn2.style.height = '30px';
     btn2.style.fontSize = '15px';
 
     div.setAttribute('class', 'pad-left');
-    input.setAttribute('class','zilla-slab-regular');
-    input.style.width = '75px';
+
+    input.style.width = '150px';
     input.style.height = '24px';
     input.style.fontSize = '24px';
 
@@ -90,21 +107,26 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    input4.addEventListener('keydown', ({key}) => {
+    input2.addEventListener('keydown', ({key}) => {
         if (key === "Enter") {
             document.getElementById('btn2').click();
         }
     });
-});
 
-var final; 
+    input3.addEventListener('keydown', ({key}) => {
+        if (key === "Enter") {
+            document.getElementById('btn3').click();
+        }
+    });
+
+});
 
 function test (myForm) {
     var thingy = myForm.input.value;
-    var input1;
-    const input4 = document.getElementById('input2');
+    var inp;
+    const input2 = document.getElementById('input2');
     const btn2 = document.getElementById('btn2');
-    const input3 = document.getElementById('input');
+    const input = document.getElementById('input');
     const btn = document.getElementById('btn');
     const p = document.getElementById('p');
     const ask = document.getElementById('ask');
@@ -112,32 +134,32 @@ function test (myForm) {
     ask.setAttribute('class', 'zilla-slab-regular');
     p.setAttribute('class', 'zilla-slab-regular');
 
-    input1 = thingy.toLowerCase();
-    console.log(input1);
-    if (['celcius', 'c'].includes(input1)) {
+    inp = thingy.toLowerCase();
+    console.log(inp);
+    if (['celcius', 'c'].includes(inp)) {
         console.log('Celcius it is!');
         ask.innerText = 'Do you want to convert C to F, or C to K?';
         btn.style.visibility = 'hidden';
-        input3.style.visibility = 'hidden';
+        input.style.visibility = 'hidden';
         p.innerText = '';
         ask.innerText = 'Do you want to convert C to F, or C to K?';
-        input4.style.visibility = 'visible';
+        input2.style.visibility = 'visible';
         btn2.style.visibility = 'visible';
-    } else if (['fahrenheit', 'f'].includes(input1)) {
+    } else if (['fahrenheit', 'f'].includes(inp)) {
         console.log('Fahrenheit it is!');
         btn.style.visibility = 'hidden';
-        input3.style.visibility = 'hidden';
+        input.style.visibility = 'hidden';
         p.innerText = '';
         ask.innerText = 'Do you want to convert F to C, or F to K?';
-        input4.style.visibility = 'visible';
+        input2.style.visibility = 'visible';
         btn2.style.visibility = 'visible';
-    } else if (['kelvin', 'k'].includes(input1)) {
+    } else if (['kelvin', 'k'].includes(inp)) {
         console.log('Kelvin it is!');
         btn.style.visibility = 'hidden';
-        input3.style.visibility = 'hidden';
+        input.style.visibility = 'hidden';
         p.innerText = '';
         ask.innerText = 'Do you want to convert K to F, or K to C?';
-        input4.style.visibility = 'visible';
+        input2.style.visibility = 'visible';
         btn2.style.visibility = 'visible';
     } else {
         p.innerText = 'Invalid input. Please enter Kelvin, Fahrenheit, or Celsius.';
@@ -146,36 +168,104 @@ function test (myForm) {
 }
 
 function test2 (myForm) {
-    var input5;
+    var inp2;
     var thingy2 = myForm.input2.value;
-    const input4 = document.getElementById('input2');
+    const input2 = document.getElementById('input2');
     const btn2 = document.getElementById('btn2');
     const p2 = document.getElementById('p2');
     const ask = document.getElementById('ask');
+    const ask2 = document.getElementById('ask2');
 
     p2.setAttribute('class', 'zilla-slab-regular');
 
-    input5 = thingy2.toLowerCase();
-    console.log(input5);
-    if (['cf', 'ck'].includes(input5)) {
+    inp2 = thingy2.toLowerCase();
+    console.log(inp2);
+    if (inp2 == 'cf') {
         p2.innerText = '';
-        if (input5 == 'cf') {
-
-        };
-        input4.style.visibility = 'hidden';
+        input2.style.visibility = 'hidden';
         btn2.style.visibility = 'hidden';
-        ask.style.visibility = 'hidden';
-    } else if (['fc', 'fk'].includes(input5)) {
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:';
+        convCtoF(myForm2);
+    } else if (inp2 == 'ck') {
         p2.innerText = '';
-        input4.style.visibility = 'hidden';
+        input2.style.visibility = 'hidden';
         btn2.style.visibility = 'hidden';
-        ask.style.visibility = 'hidden';
-    } else if (['kf', 'kc'].includes(input5)) {
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:';
+        convCtoF(myForm2);
+    } else if (inp2 == 'fc') {
         p2.innerText = '';
-        input4.style.visibility = 'hidden';
+        input2.style.visibility = 'hidden';
         btn2.style.visibility = 'hidden';
-        ask.style.visibility = 'hidden';
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:';
+        convCtoF(myForm2);
+    } else if (inp2 == 'fk') {
+        p2.innerText = '';
+        input2.style.visibility = 'hidden';
+        btn2.style.visibility = 'hidden';
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:'; 
+        convCtoF(myForm2);
+    } else if (inp2 == 'kf') {
+        p2.innerText = '';
+        input2.style.visibility = 'hidden';
+        btn2.style.visibility = 'hidden';
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:';
+        convCtoF(myForm2);
+    } else if (inp2 == 'kc') {
+        p2.innerText = '';
+        input2.style.visibility = 'hidden';
+        btn2.style.visibility = 'hidden';
+        ask.innerText = '';
+        ask2.innerText = 'Please input a degree you would like to convert:';
+        convCtoF(myForm2);
     } else {
         p2.innerText = 'Invalid input.'
-    }   
+    }
+    myForm.input2.value = ""
+}
+
+function convCtoF (myForm2) {
+    // convert formula for C to F: (deg * 1.8) + 32
+    var thingy3 = myForm2.input3.value;
+    var temp = document.getElementById('input3').value;
+    console.log(temp);
+    const input3 = document.getElementById('input3');
+    const btn3 = document.getElementById('btn3');
+    const ask2 = document.getElementById('ask2');
+    const p3 = document.getElementById('p3');
+
+    ask2.setAttribute('class', 'zilla-slab-regular');
+    p3.style.visibility = 'hidden'
+
+    input3.style.visibility = 'visible';
+    btn3.style.visibility = 'visible';
+    
+    let deg = parseFloat(thingy3);
+    let v = (deg * 1.8) + 32;
+    let vFin = Math.round(v)
+    p3.innerText = 'The fahrenheit degree is: ' + vFin;
+}
+
+function convCtoK (myForm) {
+
+}
+
+function convFtoC (myForm) {
+
+}
+
+function convFtoK (myForm) {
+
+}
+
+function convKtoF (myForm) {
+
+}
+
+function convKtoC (myForm) {
+
 }
