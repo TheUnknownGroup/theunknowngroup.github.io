d87b2d770543f24e929c14fbf98c5f11fe9ed52d
devonk15 <kaylordevon5@gmail.com>
2025-01-11 10:45:15 -0500

    oh lord

--- src/js/tempwiki.js
+++ src/js/tempwiki.js
@@ -1,5 +1,5 @@
-if (window.addEventListener){window.addEventListener('load', WindowLoad, false);} else if (window.attachEvent) {window.attachEvent('onload', WindowLoad);} function WindowLoad() {
+document.addEventListener("DOMContentLoaded", () => {
     var h1 = document.getElementById('h1');
 
     h1.setAttribute('class', 'zilla-slab-regular')
-}
+})
